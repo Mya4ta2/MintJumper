931d5aaa81ed109732cc22928d7d6aae072a01a4
Mya4ta <sefce3543@gmail.com>
2021-01-13 10:01:55 +0300

    add Owner param to Bullet constructor

--- core/src/mint/runner/type/Bullet.java
+++ core/src/mint/runner/type/Bullet.java
@@ -2,18 +2,19 @@ package mint.runner.type;
 
 import com.badlogic.gdx.math.Vector2;
 
-public class Bullet implements Entity{
+public class Bullet implements Entity {
     public Vector2 position = new Vector2();
     public Vector2 velocity = new Vector2();
     public Entity owner;
     public World world;
     public BulletType type;
 
-    public Bullet(Vector2 position, Vector2 velocity, World world, BulletType type) {
+    public Bullet(Vector2 position, Vector2 velocity, World world, BulletType type, Entity owner) {
         this.position = position;
         this.velocity = velocity;
         this.world = world;
         this.type = type;
+        this.owner = owner;
 
         world.entitys.add(this);
     }
