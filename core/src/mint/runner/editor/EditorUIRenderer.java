8de9b1e12cc1607dd00465a908b2e7e9c69cd352
Mya4ta2 <sefce3543@gmail.com>
2021-02-12 01:05:26 +0300

    world select in editor

--- core/src/mint/runner/editor/EditorUIRenderer.java
+++ core/src/mint/runner/editor/EditorUIRenderer.java
@@ -2,6 +2,7 @@ package mint.runner.editor;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
@@ -16,9 +17,16 @@ import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import mint.runner.Vars;
 import mint.runner.ctype.Renderer;
+import mint.runner.maps.WorldReader;
 import mint.runner.maps.WorldWriter;
+import mint.runner.screen.GameScreen;
 import mint.runner.type.ContentType;
+import mint.runner.type.World;
 import mint.runner.ui.*;
+
+import java.util.ArrayList;
+
+import static mint.runner.Vars.worldDir;
 
 public class EditorUIRenderer implements Renderer {
     public OrthographicCamera camera;
@@ -32,6 +40,9 @@ public class EditorUIRenderer implements Renderer {
     public Stage currentStage;
     public Stage resumeStage;
     public Stage propertiesStage;
+    public Stage worldSelectStage;
+
+    Table worldSelectTable;
 
     @Override
     public void create() {
@@ -40,7 +51,6 @@ public class EditorUIRenderer implements Renderer {
         stage = new Stage();
         resumeStage = new Stage();
         batch = new SpriteBatch();
-        currentStage = stage;
         propertiesStage = new Stage();
 
         menu = new ContentSelectMenu(ContentType.overlay, new Texture("buttonUp.png"), new Texture("buttonDown.png"));
@@ -85,6 +95,56 @@ public class EditorUIRenderer implements Renderer {
         textField.onTextChangedEvents.add(() -> {
             EditorVars.world = WorldWriter.cutWorldToSize(EditorVars.world, Integer.parseInt(textField.getText().toString()), EditorVars.world.height);
         });
+
+        //TODO i sorry =(
+        worldSelectStage = new Stage();
+        worldSelectTable = new Table();
+
+        final ArrayList<World> worlds = new ArrayList<>();
+        FileHandle worldDirHandle = Gdx.files.internal("world");
+
+        for (int i = 0; i < Gdx.files.internal(worldDir).list().length; i++) {
+            try {
+                worlds.add(WorldReader.readFile(Gdx.files.internal(worldDir).list()[i]));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        Texture buttonUp = new Texture("buttonUp.png");
+        Texture buttonDown = new Texture("buttonDown.png");
+
+        TextButton back = new TextButton(buttonUp, buttonDown, new BitmapFont());
+        back.setText("back");
+        back.setSize(170,70);
+        back.addListener(new InputListener(){
+            @Override
+            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                currentStage = stage;
+                return super.touchDown(event, x, y, pointer, button);
+            }
+        });
+
+        worldSelectTable.center().add(back).row();
+
+        for (int i = 0; i < worlds.size(); i++) {
+            TextButton worldTempButton = new TextButton(buttonUp, buttonDown,new BitmapFont());
+            worldTempButton.setText(worlds.get(i).name);
+            worldTempButton.setSize(170,70);
+            worldSelectTable.center().add(worldTempButton).row();
+            final int finalI = i;
+            worldTempButton.addListener(new InputListener(){
+                @Override
+                public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+                    EditorVars.world = worlds.get(finalI);
+                    currentStage = stage;
+                    return super.touchDown(event, x, y, pointer, button);
+                }
+            });
+        }
+
+        worldSelectStage.addActor(worldSelectTable);
+        currentStage = worldSelectStage;
     }
 
     @Override
@@ -118,6 +178,8 @@ public class EditorUIRenderer implements Renderer {
         menu.setSize(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight());
         menu.setPosition(width-menu.getWidth(),0);
         menu.setPositions();
+
+        worldSelectTable.setSize(width,height);
 
         toolSelectMenu.setPosition(0, 0);
         toolSelectMenu.setSize(Gdx.graphics.getWidth()/5, Gdx.graphics.getHeight());
